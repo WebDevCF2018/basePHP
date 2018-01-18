6a339c2e634278352c9a325ef924e68e6479760a
michaeljpitz <michaeljpitz@gmail.com>
2018-01-18 12:09:33 +0100

    08 if elseif else

--- 08-if-elseif-else.php
+++ 08-if-elseif-else.php
@@ -25,7 +25,24 @@ if($hasard<5){
     echo '$hasard est plus grand ou égal à 5 :'." $hasard<br>";
 }
 ?>
-<h2>Les opérateurs de comparaison (sans le type)</h2>
+
+<h2>La condition si, sinon si et sinon: if, elseif and else</h2>
+<p>Vérifie si une condition est vraie (true) et exécute le code entre { }, SINON SI (elseif ou else if) une autre condition est vraie et exécute le code entre { } SINON exécute le code entre { } du else</p>
+<?php
+// si hasard est plus petit ou égal à 2
+if($hasard <= 2){
+    echo 'if $hasard est <= 2 :'." $hasard<br>";
+}elseif($hasard <= 5){
+    echo 'elseif $hasard est <= 5 (mais >2) :'." $hasard<br>";
+}else if($hasard <= 7){
+    echo 'else if $hasard est <= 7 (mais >5) :'." $hasard<br>";
+}else{
+    echo 'else Dans les autres cas: ($hasard est >7) :'." $hasard<br>";
+}
+?>
+
+
+<h2>Les opérateurs de comparaison (sans le typage)</h2>
 <img src="img/compa.png" alt="comparaison"/>
 </body>
 </html>
