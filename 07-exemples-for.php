a6a8075d2e65f9cdc26477a29dd35c65c43f6b22
michaeljpitz <michaeljpitz@gmail.com>
2018-01-18 11:27:53 +0100

    07 corrige

--- 07-exemples-for.php
+++ 07-exemples-for.php
@@ -9,7 +9,7 @@
 <p>La boucle for peut être utilisée pour afficher les éléments d'un tableau indexé non modifié. Elle ne fonctionne pas avec les tableaux relatifs (on utilisera alors foreach ou les variantes de while).</p>
 <?php
 // tableau indexé
-$tabIndex = ["un",'deux',"trois","quatre","cinq","six","sept","huit","neuf","dix","onze","douze"];
+$tabIndex = ["un",'deux',"trois","quatre","cinq","six","sept","huit","neuf","dix","onze","douze","treize"];
 
 // on va compter le nombre d'entrées dans le tableau ! count(array) commence à compter à 1
 $compte = count($tabIndex);
@@ -18,6 +18,19 @@ $compte = count($tabIndex);
 for($i=0; $i<$compte; $i++){
     echo $tabIndex[$i]." ";
 }
+echo "<hr>";
+
+// exercice : affichez les éléments du tableau par ordre inversé
+for($i=$compte-1; $i>=0; $i--){
+    echo $tabIndex[$i]." ";
+}
+echo "<hr>";
+
+// exercice 2 : affichez que les chiffres paires (valeurs) du tableau (deux quatre six ...)
+for($i=1; $i<$compte; $i+=2){
+    echo $tabIndex[$i]." ";
+}
+echo "<hr>";
 ?>
 </body>
 </html>
