2e9846220a9c4e7d15439fdaa9d8861c85ba154b
michaeljpitz <michaeljpitz@gmail.com>
2018-01-18 11:54:00 +0100

    opérateurs comparaison

---
+++ 08-if-elseif-else.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Les conditions if - elseif et else</title>
+</head>
+<body>
+<h1>Les conditions if - elseif et else</h1>
+<h2>La condition simple: if</h2>
+<p>Vérifie si une condition est vraie (true) et exécute le code entre { }</p>
+<?php
+// chiffre au hasard entre 1 et 10
+$hasard = mt_rand(1,10);
+// si le chiffre trouvé au hasard est plus petit que 5
+if($hasard<5){
+    echo '$hasard est plus petit que 5 :'." $hasard<br>";
+}
+?>
+<h2>La condition si, sinon: if and else</h2>
+<p>Vérifie si une condition est vraie (true) et exécute le code entre { } SINON exécute le code entre { } du else</p>
+<?php
+if($hasard<5){
+    echo '$hasard est plus petit que 5 :'." $hasard<br>";
+}else{
+    echo '$hasard est plus grand ou égal à 5 :'." $hasard<br>";
+}
+?>
+<h2>Les opérateurs de comparaison (sans le type)</h2>
+<img src="img/compa.png" alt="comparaison"/>
+</body>
+</html>
