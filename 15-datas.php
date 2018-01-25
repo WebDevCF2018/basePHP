a0f9429e213d545b9d7e5f5a03691596a89a7557
michaeljpitz <michaeljpitz@gmail.com>
2018-01-25 16:17:03 +0100

    14-variables-GET

---
+++ 15-datas.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Données mises manuellement dans un tableau
+ */
+
+// titre avec id (clef)
+$datasTitre = array(1 => "Volkswagen a gagné",
+                    2 => "La réforme du bac amorce la refonte du lycée ",
+                    3 => "Lula, un nouveau procès explosif ",
+                    7 => "Le Brésil perd le contrôle de ses prisons ");
+// texte avec id (clef)
+$datasTxt = array(1 => "Alors que les clients américains et canadiens ont été dédommagés par Volkswagen, l’immense majorité des clients européens n’aura rien. À peine 100 000 plaintes devraient être déposées en Europe, sur près de 8 millions de voitures Volkswagen équipées du fameux logiciel truqué. « C'est un scandale », dénonce l’avocat Christopher Rother dans un entretien à Mediapart. Explications.",
+                2=> "Le rapport sur la réforme du baccalauréat remis ce 24 janvier au ministre de l’éducation nationale préconise de supprimer les séries L, ES ou S pour faire passer cinq grandes épreuves aux lycéens. Une part importante est laissée au contrôle continu. Mais il propose surtout de repenser toute l’architecture du lycée.",
+                3 => "Jugé en appel à partir de mercredi, Lula espère atteindre la date charnière du 15 août, jour de clôture du dépôt des candidatures à l’élection présidentielle, sans condamnation définitive qui l’empêcherait de se présenter. Le Parti des travailleurs se refuse publiquement à envisager un plan B.",
+                7 => "Avec la troisième population carcérale au monde, le pays est confronté à la prise en main des établissements pénitentiaires par les gangs. La lutte qu’ils se livrent pour leur contrôle s’est une nouvelle fois exprimée en début d’année, malgré la création d’unités policières d’élite.");
--- 15-get-include.php
+++ 15-get-include.php
@@ -1,11 +1,32 @@
+<?php
+// on importe nos données au format array depuis 15-datas.php
+include "15-datas.php";
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>titre</title>
+    <title>Exemple avec variables get</title>
 </head>
 <body>
+<h1>Notre site</h1>
+<h2>Exemples venant de 15-datas.php</h2>
+<a href="15-get-include.php">Accueil</a> |
 <?php
+// tant q'on a des éléments dans le tableau $datasTitre, on récupère sa clef ($key) et sa valeur (=> $values)
+foreach ($datasTitre as $key => $values){
+    // on crée les liens avec la variable de type GET 'id' remplie avec la clef du tableau, et la valeur comme texte du lien
+    echo "<a href='?id=$key'>$values</a> | ";
+}
+
+// condition si il n'existe pas de variable GET 'id'
+if(!isset($_GET['id'])){
+    echo "<h3>Page d'accueil</h3>";
+}else{
+    echo "<h3>".$datasTitre[$_GET['id']]."</h3>";
+    // sinon on affiche le texte venant du tableau $datasTxt en utilsant la variable GET 'id' comme clef (correspondance des clefs des 2 tableaux)
+    echo "<p>".$datasTxt[$_GET['id']]."</p>";
+}
 
 ?>
 </body>
