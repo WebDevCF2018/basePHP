972d786b42b2cc6bfe56fff557667e6e4e67d885
michaeljpitz <michaeljpitz@gmail.com>
2018-01-26 10:59:06 +0100

    Opérateurs logiques

---
+++ 17-and-or-xor.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Opérateurs logiques</title>
+</head>
+<body>
+<h1>Opérateurs logiques</h1>
+<img src="img/operateurs-logiques.png" alt="Opérateurs logiques" /><br>
+<?php
+
+?>
+</body>
+</html>
