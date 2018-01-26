3b141d32554845e96ae9d24137ae1ae6722a6663
michaeljpitz <michaeljpitz@gmail.com>
2018-01-26 10:29:51 +0100

    foreach final

--- 16-foreach.php
+++ 16-foreach.php
@@ -22,7 +22,41 @@
 <p>La première forme passe en revue le tableau array_expression. À chaque itération, la valeur de l'élément courant est assignée à $value et le pointeur interne de tableau est avancé d'un élément (ce qui fait qu'à la prochaine itération, on accédera à l'élément suivant).</p>
 <p>La seconde forme assignera en plus la clé de l'élément courant à la variable $key à chaque itération.</p>
 <?php
+// tableau indexé
+$cookie = array("Chocolat","sucre","farine","lait",);
+// tableau associatif
+$muffin = [ "sucre"  => "100g",
+            "farine" => "280g",
+            "oeufs"  => 2,
+            "lait"   => "150ml",
+            "beurre" => "80g",
+          ];
+// foreach sans clef sur cookie (récupération valeur)
+foreach ($cookie as $valeur){
+    echo "$valeur | ";
+}
+echo "<hr>";
+// foreach avec clef sur cookie (récupération clef ET valeur)
+foreach ($cookie as $clef => $valeur){
+    echo "$clef : $valeur | ";
+}
 
+echo "<hr>";
+
+// foreach sans clef sur muffin (récupération valeur)
+foreach ($muffin as $valeur){
+    echo "$valeur | ";
+}
+echo "<hr>";
+// foreach avec clef sur muffin (récupération clef ET valeur)
+foreach ($muffin as $clef => $valeur){
+    echo "$clef : $valeur | ";
+}
+echo "<hr>";
+
+
+echo"<pre>débuggage: \n\r";
+echo var_dump($cookie,$muffin)."</pre>";
 ?>
 </body>
 </html>
