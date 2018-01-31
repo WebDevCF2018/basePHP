fb6f1a2c3436a2a00ca68f0ebd28096d18c28815
michaeljpitz <michaeljpitz@gmail.com>
2018-01-31 12:18:53 +0100

    début exe switch

---
+++ 21-exe-switch.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>exe switch</title>
+</head>
+<body>
+<h1>exe switch</h1>
+<h2>Liste de mots acceptés</h2>
+<h3>haha | lulu | triste | jovial</h3>
+<form action="" name="monForm" method="get">
+    <input type="text" name="q" placeholder="Entrez un mot">
+    <input type="submit" value="Envoyer">
+</form>
+<p>Si l'utilisateur entre</p>
+</body>
+</html>
