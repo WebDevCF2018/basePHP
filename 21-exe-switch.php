1a44ec9c5ecdccda1091688ee930d94ee466cc28
Pitz Michaël <michaeljpitz@gmail.com>
2018-02-01 12:28:50 +0100

    exe 21 switch

--- 21-exe-switch.php
+++ 21-exe-switch.php
@@ -12,6 +12,15 @@
     <input type="text" name="q" placeholder="Entrez un mot">
     <input type="submit" value="Envoyer">
 </form>
-<p>Si l'utilisateur entre</p>
+<h4>En utilisant les variables GET et le switch</h4>
+<p>Affichez ci-dessous :</p>
+<p><?php
+
+?></p>
+<p>Si l'utilisateur entre quelque chose qui n'est pas dans la liste, affichez $letexte." n'est pas dans la liste".</p>
+<p>Si l'utilisateur entre "haha", affichez "Qu'est-ce qui te fait rire?"</p>
+<p>Si l'utilisateur entre "lulu", affichez "lala?"</p>
+<p>Si l'utilisateur entre "triste", affichez des numériques de 1 à 200 séparés par un " | "</p>
+<p>Si l'utilisateur entre "jovial", affichez la date et l'heure actuelle au format (Y-m-d H:i:s => 2018-02-01 12:28:15)</p>
 </body>
 </html>
