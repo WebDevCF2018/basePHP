e0023e570784802aa5a2be9695d857158fe48e05
michaeljpitz <michaeljpitz@gmail.com>
2018-02-23 11:24:42 +0100

    31

--- 31-config.php
+++ 31-config.php
@@ -4,7 +4,7 @@
  */
 define("DB_HOST","localhost");
 define("DB_PORT",3306);
-define("DB_NAME","affiche");
+define("DB_NAME","exe_31");
 define("DB_LOGIN","root");
 define("DB_PWD","");
 define("DB_CHARSET","utf8");
--- 31-insert-select-form.php
+++ 31-insert-select-form.php
@@ -5,18 +5,43 @@ require_once "31-config.php";
 // connexion à la DB en mysqli procédural, arrêt en cas d'erreur de connexion
 require_once "mysqliconnect.php";
 
-//
+// requête sql qui va récupérer tous les 'livre' avec le 'nom' et 'prenom' de leur 'auteur' et l' 'url' de l'image
+$sql = "SELECT l.*, a.nom, a.prenom, i.url
+	        FROM livre l
+		INNER JOIN auteur a
+			ON l.auteur_idauteur = a.idauteur
+		INNER JOIN image i
+			ON l.idlivre = i.livre_idlivre;";
+
+// on effectue la requête
+$recup = mysqli_query($mysqli,$sql);
+
+// on compte le nombre de ligne de résultat
+$compte = mysqli_num_rows($recup);
 
 ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>titre</title>
+    <title>Nos livres</title>
 </head>
 <body>
+<h1>Nos livres</h1>
 <?php
-
+// si on a pas encore de livre
+if($compte==0){
+    echo "<h3>Pas encore de livre!</h3>";
+}else{
+    while ($a = mysqli_fetch_assoc($recup)){
+        ?>
+    <h3><?=$a['titre']?></h3>
+    <img src="<?=$a['url']?>" width='100px' height='100px' alt='' />
+    <p><?=$a['description']?></p>
+    <p>Ecrit en <?=substr($a['parution'],0,-6)?> par <?=$a['prenom']?> <?=$a['nom']?></p>
+        <?php
+    }
+}
 ?>
 </body>
 </html>
