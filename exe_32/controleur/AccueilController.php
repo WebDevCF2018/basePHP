eca932257933bbe4f2e097251022e266f7a4887a
michaeljpitz <michaeljpitz@gmail.com>
2018-03-01 11:42:44 +0100

    exe_32 menu accueil ok

--- exe_32/controleur/AccueilController.php
+++ exe_32/controleur/AccueilController.php
@@ -8,7 +8,7 @@ require_once "modeles/CategModel.php";
 
 // on récupère le menu depuis CategModel
 $pour_menu = listeMenu($mysqli);
-var_dump($pour_menu);
+// var_dump($pour_menu);
 
 // on prend la vue
 require_once "vues/Accueil.html.php";
--- exe_32/vues/menu.php
+++ exe_32/vues/menu.php
@@ -1,4 +1,10 @@
 <ul>
     <li><a href="./">Accueil</a></li>
-    <li><a href=""></a></li>
+    <?php
+    foreach ($pour_menu AS $itemMenu) {
+    ?>
+        <li><a href="?c=<?=$itemMenu['slug']?>"><?=$itemMenu['titre']?></a></li>
+    <?php
+    }
+    ?>
 </ul>
